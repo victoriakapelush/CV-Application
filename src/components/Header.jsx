ece4ac36f44323f937815384fb87f9b3fce2e881
victoriakapelush <victoriakapelush@gmail.com>
2024-08-16 02:29:31 -0400

    Rename pdf file btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 import React, { useRef } from 'react';
 import '../App.css';
 import { SaveToPDF } from './SaveToPDF';
+import pdf from '../assets/file.png'
 
 export default function Header({ contentRef }) {
     const generatePDF = SaveToPDF(contentRef); // Get the PDF generation function
@@ -10,7 +11,7 @@ export default function Header({ contentRef }) {
             <div className='app-name flex-row'>
                 <h1 id='resume-builder-name'>Resume Builder</h1>
                 <div className='image-div flex-row'>
-                    <img src='./src/assets/file.png' onClick={generatePDF}></img>
+                    <img src={pdf} onClick={generatePDF}></img>
                 </div>
             </div>
         </div>
